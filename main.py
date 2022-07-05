a03d9ad0b932d089464ae44222f33156c78f838c
Mansour Alh <mansouralhussain94@gmail.com>
2022-07-05 19:58:57 +0300

    done

--- main.py
+++ main.py
@@ -3,7 +3,10 @@
 # - Prints every element of the list
 def printer(elements):
     # Your code here
-    ...
+    
+    for element in elements :
+        print("this is element"+element)
+
 
 
 # to_celsius(temperatures)
@@ -15,8 +18,14 @@ def printer(elements):
 #   C = (F - 32) * (5/9)
 def to_celsius(temperatures):
     # Your code here
-    ...
-
+    
+   
+    tempinc=[]
+   
+    for tempc in temperatures:
+        tempinc.append ((tempc-32)*5/9)
+    return tempinc
+   
 
 # hottest_days(temperatures, threshold)
 # - Accepts a list of temperatures
@@ -25,9 +34,16 @@ def to_celsius(temperatures):
 #   that exceed the threshold
 def hottest_days(temperatures, threshold):
     # Your code here
-    ...
+    exceed= []
+    for temp in temperatures:
+        if  temp> threshold:
+            return temp
+        exceed.append(temp)
+    return exceed
 
-
+    
+    
+   
 # log_hottest_days(temperatures, threshhold)
 # - Accepts a list of temperatures
 #   IN DEGREES FAHRENHEIT
@@ -39,4 +55,10 @@ def hottest_days(temperatures, threshold):
 #       all previous functions
 def print_hottest_days(temperatures, threshhold):
     # Your code here
-    ...
+    tempsinc = []
+    for tempsc in temperatures:
+        
+        if (tempsc > threshhold):
+            return tempsc
+        tempsinc.append((tempsc-32)*5/9)
+    print (tempsinc)
